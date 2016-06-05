3769bcde84dca9abba7ee90b6c8f0e792203acd3
rcrespop <robertocre@gmail.com>
2016-06-05 22:19:13 +0200

    First version

---
+++ microservices-HelloWord/registration/src/main/java/net/robertocrespo/microservices/registration/RegistrationServer.java
@@ -0,0 +1,26 @@
+package net.robertocrespo.microservices.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * All you need to run a Eureka registration server.
+ * 
+ * @author Roberto Crespo
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistrationServer {
+
+	/**
+	 * Run the application using Spring Boot and an embedded servlet engine.
+	 * 
+	 * @param args
+	 *            Program arguments - ignored.
+	 */
+	public static void main(String[] args) {
+
+		SpringApplication.run(RegistrationServer.class, args);
+	}
+}
